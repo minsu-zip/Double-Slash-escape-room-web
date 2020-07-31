52bc2d051d9bcf9d1530b959746b2c9aaf1e9681
unknown <jeehea99@naver.com>
2020-08-01 02:54:44 +0900

    [변경]:공통 헤더 기본 틀

--- src/components/Common/Header/index.jsx
+++ src/components/Common/Header/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import * as S from './style.js'
+
 const Header = () => {
   return (
     <>
-      <div>Header</div>
+    <S.Container>
+      
+      <S.Name><a href='/'>방탈출</a></S.Name>
+
+      <S.MenuWrapper>
+        <S.Menu><a href='/'>카페</a></S.Menu>
+        <S.Menu><a href='/'>테마</a></S.Menu>
+        <S.Menu><a href='/'>마이페이지</a></S.Menu>
+      </S.MenuWrapper>
+      
+    </S.Container>
     </>
   );
 };
---
+++ src/components/Common/Header/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+//components/Common/Header/style.js
+
+//전체 감쌈
+export const Container = styled.header `
+    display: absolute;
+    top: 0px
+    position: fixed;
+    padding: 5px 10px 5px 10px;
+    margin: 0px;
+    background-color:linen;
+`;
+
+//서비스 이름
+export const Name = styled.div `
+    display: inline-block;
+    padding: 5px 10px;
+    margin: 10px 10px;
+    background-color:skyblue;
+`;
+
+//메뉴묶음
+export const MenuWrapper = styled.div `
+    display: inline-block;
+    float: right;
+    margin: 5px 10px 5px 10px; 
+`;
+
+//메뉴개별
+export const Menu = styled.span `
+    display: inline-block;
+    padding: 5px;
+    margin: 5px 20px;
+    background-color:skyblue;
+`;
