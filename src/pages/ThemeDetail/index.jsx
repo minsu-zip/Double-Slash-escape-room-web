a74e7407b0d43595536b9de53ee35992b95a886d
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-08-04 01:12:25 +0900

    [추가] : 게시글 상세 컴포넌트를 pages/themeDetail에 적용

--- src/pages/ThemeDetail/index.jsx
+++ src/pages/ThemeDetail/index.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-const ThemeDetail = () => {
+// components
+import MainSection from "../../components/ThemeDetail/MainSection";
+import DescSection from "../../components/ThemeDetail/DescSection";
+import ReviewSection from "../../components/ThemeDetail/ReviewSection";
+
+// action
+import { LOAD_THEME_REQUEST } from "../../store/actions/themeAction";
+
+// style
+import * as S from "./style";
+
+const ThemeDetail = ({ match }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const { params } = match;
+    dispatch({
+      type: LOAD_THEME_REQUEST,
+      data: {
+        params,
+      },
+    });
+  }, [dispatch, match]);
+
   return (
     <>
-      <div>ThemeDetail</div>
+      <S.Layout>
+        <MainSection />
+        <DescSection />
+        <ReviewSection />
+        <section></section>
+      </S.Layout>
     </>
   );
 };
