d7f8653d173bc82a8b7816198ba793011c9bd0c4
minsu-zip <alstn4814@gmail.com>
2020-08-06 00:43:38 +0900

    [변경]OffsetCardList파일 컴포넌트 배열 push->map

--- src/components/Common/OffsetCardList/index.jsx
+++ src/components/Common/OffsetCardList/index.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import Post from '../Post';
 import * as S from './style.js';
-
+import dummy from '../../../dummy/themes.json';
 
 const OffsetCardList = () => {
     const [showResults, setShowResults] = React.useState(false);
     const onClick = () => setShowResults(true);
-    const slides_s = [];
+    
+    // const slides_s = [];
+    // for (let i = 0; i < 4; i++) {
+    //     slides_s.push(
+    //         <S.Column>
+    //             <S.Item>
+    //                 <S.Img><Post></Post><S.text>찜</S.text></S.Img>
+    //                 <div>설명추가</div>
+    //             </S.Item>
+    //         </S.Column>
+    //     )
+    // }
 
-    for (let i = 0; i < 4; i++) {
-        slides_s.push(
-        <S.Column>
-            <S.Item>
-                <S.Img><Post></Post><S.text>찜</S.text></S.Img>
-                <div>설명추가</div>
-            </S.Item>
-        </S.Column>
+    const datadummy = dummy;
+    const slides = datadummy.map((dummy, index) => {
+        return (
+            <S.Column ket={index}>
+                <S.Item>
+                    <S.Img><Post></Post><S.text>찜</S.text></S.Img>
+                    <div>설명추가</div>
+                </S.Item>
+            </S.Column>
         )
-      }
+    })
 
     const Results = () => (
         <div id="results" className="search-results">
             <S.Row>
-                {slides_s}
+                {slides}
             </S.Row>
         </div>
     )
@@ -31,9 +43,9 @@ const OffsetCardList = () => {
         <>
             <S.Container>
                 <S.Row>
-                    {slides_s}
+                    {slides}
                 </S.Row>
-                
+
                 {showResults ? <Results /> : null}
 
             </S.Container>
