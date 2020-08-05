9f4e2318828e4803caebd02d38f59e4547b92d1f
unknown <jeehea99@naver.com>
2020-08-05 22:09:38 +0900

    [변경]:테마 헤더 검색창, 필터버튼 추가

--- src/components/Common/Footer/index.jsx
+++ src/components/Common/Footer/index.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
       <S.Container>
         Copyright 2020. 더블슬래시 방탈출팀. All rights reserved. ｜ 제휴,문의 : 000-0000-0000 ooo@oooo.com
       </S.Container>
-      {/* 제작 : 기획팀 000 디자인팀 000 000 개발팀 000 */}
     </>
   );
 };
