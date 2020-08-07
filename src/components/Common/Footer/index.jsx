f960008316ff62ef2a260494a16b301d64846208
unknown <jeehea99@naver.com>
2020-08-07 22:56:14 +0900

    [수정]:푸터 fixed로 수정했던 것 취소

--- src/components/Common/Footer/index.jsx
+++ src/components/Common/Footer/index.jsx
@@ -5,7 +5,7 @@ import * as S from './style.js'
 const Footer = () => {
   return (
     <>
-      <S.Container><div></div>
+      <S.Container>
         Copyright 2020. 더블슬래시 방탈출팀. All rights reserved. ｜ 제휴,문의 : 000-0000-0000 ooo@oooo.com
       </S.Container>
     </>
--- src/components/Common/Footer/style.js
+++ src/components/Common/Footer/style.js
@@ -2,9 +2,6 @@ import styled from 'styled-components';
 //components/Common/Footer/style.js
 
 export const Container = styled.footer `
-    
-    bottom: 0;
-    position: fixed;
     width: 100%;
     
     padding: 10px 30px;
@@ -13,10 +10,4 @@ export const Container = styled.footer `
     background-color:linen;
     font-size: 12px;
     text-align: center;
-`;//display: block;
-
-
-// div {
-//         position: relative;
-//         min-height:100%
-//     }
+`;
