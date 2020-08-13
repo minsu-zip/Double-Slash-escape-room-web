8963cbcb7e5448ade296825153d4eddf7d796b95
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-08-13 14:40:16 +0900

    [변경] : 필터링 전송 redux 추가적으로 변경

--- src/store/actions/filterAction.js
+++ src/store/actions/filterAction.js
@@ -4,16 +4,19 @@ export const printFilterPopup = () => ({ type: PRINT_FILTER_POPUP });
 
 // 필터링 요소 버튼 클릭
 export const CLICK_FILTER_CHECK_BOX = "CLICK_FILTER_CHECK_BOX";
-export const clickFilterCheckBox = (data) => ({
+export const clickFilterCheckBox = (category, name) => ({
   type: CLICK_FILTER_CHECK_BOX,
-  data,
+  data: {
+    category,
+    name,
+  },
 });
 
 // 필터링 결과 전송
 export const SUBMIT_FILTERING_REQUEST = "SUBMIT_FILTERING_REQUEST";
 export const SUBMIT_FILTERING_SUCCESS = "SUBMIT_FILTERING_SUCCESS";
 export const SUBMIT_FILTERING_FAILURE = "SUBMIT_FILTERING_FAILURE";
-export const submitFilteringRequest = (data) => ({
+export const submitFilteringRequest = (list) => ({
   type: SUBMIT_FILTERING_REQUEST,
-  data,
+  data: list,
 });
--- src/store/reducers/filterReducer/clickFilterCheckBox.js
+++ src/store/reducers/filterReducer/clickFilterCheckBox.js
@@ -1,7 +1,29 @@
+export const genre = {
+  드라마: 1,
+  아케이드: 2,
+  판타지: 3,
+  감성: 4,
+  코미디: 5,
+  모험: 6,
+  스릴러: 7,
+  음악: 8,
+  야외: 9,
+  추리: 10,
+  미스터리: 11,
+  연애: 12,
+  미션: 13,
+  잠입: 14,
+  역사: 15,
+  SF: 16,
+  성인: 18,
+};
+
 export default function clickFilterCheckBox(draft, data) {
-  if (draft.clickedList.includes(data)) {
-    draft.clickedList = draft.clickedList.filter((value) => value !== data);
+  const { category, name } = data;
+  if (draft.clickedList[category] === name) {
+    draft.clickedList[category] =
+      typeof draft.clickedList[category] === "string" ? "" : 0;
   } else {
-    draft.clickedList.push(data);
+    draft.clickedList[category] = category === "genre" ? genre[name] : name;
   }
 }
